da714e74190565ece2908ec8c8c67b7eed8e762b
Bella Cruz <bella.v.cruz@ou.edu>
2023-09-22 12:26:16 -0500

    Added for Lab5

--- R/calculate.R
+++ R/calculate.R
@@ -7,6 +7,7 @@
 #' @export
 #'
 #' @examples
+#' TSS(ddt <- read.csv("C:/Users/bella/Documents/School Folders/Fall 2023/Applied Statistical Methods/DATAxls/DDT.csv"), ddt$Height)
 TSS = function(df, value){
 
   with(df,sum((value-mean(value))^2))
